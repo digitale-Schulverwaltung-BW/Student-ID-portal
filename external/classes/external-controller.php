984c0a8cdb0a4d49ce05e3e8630285fb2d5024d1
Joerg Seyfried <joerg.seyfried@hhs.karlsruhe.de>
2024-05-13 12:25:21 +0200

    Cleanup external

--- external/classes/external-controller.php
+++ external/classes/external-controller.php
@@ -3,33 +3,75 @@ require_once('config.php');
 require_once('student.php');
 class ExternalController
 {
-    function v($f3, $args)
+    function check($f3, $stud)
     {
-        $stud = new CStudent($args['id']);
-        $template=new Template;
         if (!$stud->is_valid()) {
             $f3->set('valid', 'false');
             $f3->set('validity', 'Ungültiger Ausweis!');
             $f3->set('color', 'red');
             $f3->set('card_ID', '0000');
-            echo $template->render('templates/head.html');
             echo $template->render('templates/verify.html');
-            echo $template->render('templates/foot.html');
             exit ();
         }
+
+    }
+    function v($f3, $args)
+    {
+        $stud = new CStudent($args['id']);
+        $template=new Template;
+        $this->check($f3, $stud);
         $f3->set('valid', 'true');
         $f3->set('validity', 'Gültiger Ausweis!');
         $f3->set('color', 'green');
         $f3->set('card_ID', $stud->get_short_id());
-        echo $template->render('templates/head.html');
         echo $template->render('templates/verify.html');
-        echo $template->render('templates/foot.html');
                 //echo "Gültiger Ausweis! Nachname: $data[lastname], Vorname: $data[firstname]";
     }
     function r($f3, $args)
     {
+        $stud = new CStudent($args['id']);
+        $template=new Template;
+        if (!$stud->is_valid()) {
+            $f3->set('valid', 'false');
+            echo $template->render('templates/register.html');
+            exit ();
+        }
+        $f3->set('valid', 'true');
+        $f3->set('base', $_SERVER['REQUEST_URI']);
+        $f3->set('deploy_error', '');
         
+        echo $template->render('templates/register.html');
     }
-
+    function deploy($f3, $args)
+    {
+        $stud = new CStudent($args['id']);
+        $template=new Template;
+        $f3->set('deploy_error', '');
+        $this->check($f3, $stud);
+        switch ($args['deploy']) {
+            case 'google':
+                // request Google wallet and forward
+                echo "Google";
+                break;
+            case 'apple':
+                // request Apple wallet and forward
+                //$data = json_decode($stud->register_pass('apple'));
+                //header ('Location: '.$data['apple']);
+                //exit();
+                break;
+            case 'pdf':
+                // request PDF wallet and forward
+                echo "PDF";
+                break;
+            default:
+                // unknown deploy type        
+                $f3->set('valid', 'true');
+                $f3->set('deploy_error', 'Fehler: Unbekannter Wallet-Typ.');
+                echo $template->render('templates/head.html');
+                echo $template->render('templates/register.html');
+                echo $template->render('templates/foot.html');
+                break;
+            }        
+    }
 }
 ?>
--- external/index.php
+++ external/index.php
@@ -5,6 +5,6 @@ $f3 = require('lib/base.php');
 $f3->set('AUTOLOAD','classes/');
 $f3->set('DEBUG',3);
 $f3->route('GET /@action/@id','ExternalController->@action');
-$f3->route('GET /@action/@id/@deploy','ExternalController->@action');
+$f3->route('GET /@action/@id/@deploy','ExternalController->deploy');
 $f3->run();
 ?>
